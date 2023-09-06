ca6ea5d97ac64141016299eaf6b93e6379067e2a
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2023-09-06 16:56:14 +0100

    event binding

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -2,28 +2,43 @@ import FormCourse from "./FormCourse";
 import ListCourses from "./ListCourses";
 import ListCoursesGrid from "./ListCoursesGrid";
 
+import { useState } from "react"
+
 const Courses = () => {
 
     const title = "Blog of Coding Tech"
 
-    const courses = [
+    const dataCourses = [
         {id: 1, label: "Spring boot", image:"https://picsum.photos/id/870/200/300?blur=2"},
         {id: 2},
         {id: 3, label: "Django", image:"https://picsum.photos/id/872/200/300?blur=2"},
         {id: 4, label: "Laravel", image:"https://picsum.photos/id/873/200/300?blur=2"},
     ]
 
+    
+
+    const [courses, setCourses] = useState(dataCourses)
+
+    const save = (course) => {
+
+        setCourses([...courses, course])
+
+    }
+
     return ( 
         <>
             <div className="row my-4">
                 <div className="col-md-6">
                     <h1>{title}</h1>
+                   
                 </div>
                 <div className="col-md-4 text-end"><button className="btn btn-success btn-sm">New</button></div>
             </div>
             <div className="row my-4">
                 <div className="col-md-4 mx-auto">
-                   <FormCourse></FormCourse>
+                
+                   <FormCourse saveCourse={save}></FormCourse>
+            
                 </div>
             </div>
             <div className="row my-1">
--- src/components/courses/FormCourse.js
+++ src/components/courses/FormCourse.js
@@ -1,15 +1,38 @@
-const FormCourse = () => {
+import { useState } from 'react'
+const FormCourse = ({saveCourse}) => {
+
+    const [label, setLabel] = useState("")
+    const [image, setImage] = useState("")
+
+    const labelField = (event) => {
+        setLabel(event.target.value)
+        
+    }
+
+    const imageField = (event) => {
+        setImage(event.target.value)
+        
+    }
+
+    const transfert = () => {
+        saveCourse({label, image})
+        setLabel('')
+        setImage('')
+    }
+
     return ( 
         <>
-            <form action="">
-                <div className="form-group">
-                    <label htmlFor="">Course</label>
-                    <input type="text" name="" id="course" className="form-control" />
-                </div>
-                <div className="d-grid my-2">
-                    <button className="btn btn-primary">Add Course</button>
-                </div>
-            </form>
+             <div className="form-group">
+                        <label htmlFor="course">Course</label>
+                        <input value={label} onChange={labelField} type="text" id="course" placeholder="add a label" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="image">Image</label>
+                        <input value={image} onChange={imageField} type="url" id="image" placeholder="add a picture" className="form-control" />
+                    </div>
+                    <div className="d-grid my-2">
+                        <button onClick={transfert} className="btn btn-primary">Add Course</button>
+                    </div>
         </>
      );
 }
